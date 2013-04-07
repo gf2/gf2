dc484c39f74d705b7684ea02bed6044acf326e24
charliezhang <zero891109@gmail.com>
2013-04-07 13:17:42 -0700

    Audio upload/play and jinja

---
+++ web/configs.py
@@ -0,0 +1,16 @@
+'''
+Global configs
+
+Created on Apr 7, 2013
+
+@author: charliez
+'''
+
+DEBUG = True
+
+uri_map = {
+    "upload_audio_form" : r'/upload_audio_form',
+    "upload_audio" : r'/upload_audio',
+    "play_audio" : r'/play_audio/([^/]+)?',
+    "home_page": r'/'
+}
