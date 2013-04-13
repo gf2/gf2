923a875a737b51e3ccace71c7972f27c968147c9
charliezhang <zero891109@gmail.com>
2013-04-13 11:34:11 -0700

    indent 2

--- web/handlers/audio.py
+++ web/handlers/audio.py
@@ -11,32 +11,32 @@ from google.appengine.ext.webapp import blobstore_handlers
 from configs import *
 
 class Audio(db.Model):
-    blob_key = blobstore.BlobReferenceProperty()
+  blob_key = blobstore.BlobReferenceProperty()
 
 class AudioUploadFormHandler(webapp.RequestHandler):
-    def get(self):
-        upload_url = blobstore.create_upload_url('/upload_audio')
-        # The method must be "POST" and enctype must be set to "multipart/form-data".
-        self.response.out.write('<html><body>')
-        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-        self.response.out.write('''Upload File: <input type="file" name="file"><br> <input type="submit"
-            name="submit" value="Submit"> </form></body></html>''')
+  def get(self):
+    upload_url = blobstore.create_upload_url('/upload_audio')
+    # The method must be "POST" and enctype must be set to "multipart/form-data".
+    self.response.out.write('<html><body>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write('''Upload File: <input type="file" name="file"><br> <input type="submit"
+        name="submit" value="Submit"> </form></body></html>''')
 
 class AudioUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-    def post(self):
-        try:
-            upload = self.get_uploads()[0]
-            audio = Audio(blob_key=upload.key())
-            db.put(audio)
-            self.redirect('/play_audio/%s' % upload.key())
-        except Exception as e:
-            self.response.out.write('upload failed.')
-            if DEBUG:
-                self.response.out.write(str(e))
+  def post(self):
+    try:
+      upload = self.get_uploads()[0]
+      audio = Audio(blob_key=upload.key())
+      db.put(audio)
+      self.redirect('/play_audio/%s' % upload.key())
+    except Exception as e:
+      self.response.out.write('upload failed.')
+      if DEBUG:
+        self.response.out.write(str(e))
 
 class AudioPlayHandler(blobstore_handlers.BlobstoreDownloadHandler):
-    def get(self, audio_key):
-        if not blobstore.get(audio_key):
-            self.error(404)
-        else:
-            self.send_blob(audio_key)
+  def get(self, audio_key):
+    if not blobstore.get(audio_key):
+      self.error(404)
+    else:
+      self.send_blob(audio_key)
