89ad36468e5b72675f13393fa90ad9c2ffdd67f9
Nicholas <nicholas.zhaoyu@gmail.com>
2013-04-20 09:33:30 -0700

    Fix build.

---
+++ web/app/web/components/section_listening/script.dart
@@ -0,0 +1,6 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+class ListeningSectionComponent extends WebComponent {
+  //TODO(Nicholas): Add logic here.
+}
---
+++ web/app/web/components/section_reading/script.dart
@@ -0,0 +1,6 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+class ReadingSectionComponent extends WebComponent {
+  //TODO(Nicholas): Add logic here.
+}
