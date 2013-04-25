8dc0dd22d117d4ea0c2c6a256d028710bd916942
charliezhang <zero891109@gmail.com>
2013-04-24 23:37:16 -0700

    Some UI updates

---
+++ web/app/lib/components/home_page/script.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+import 'package:app/gf2lib.dart';
+
+class HomePage extends WebComponent {
+ 
+  void created() {
+    var user = UserManager.getUser();
+    if (user == null) {
+      window.location.href = '/';
+    }
+    window.onHashChange.listen(handleHashChange);
+  }
+  
+  handleHashChange(Event e) {
+    
+  }
+}
--- web/app/lib/components/navbar/script.dart
+++ web/app/lib/components/navbar/script.dart
@@ -4,17 +4,35 @@ import 'package:web_ui/web_ui.dart';
 import 'package:app/gf2lib.dart';
 
 class NavbarComponent extends WebComponent {
+  Element testTab = null;
+  Element practiceTab = null;
+  
   void onLoginClick(MouseEvent event) {
   }
   
   void created() {
-    var user = UserManager.getUser();
-    if (user == null) {
-      window.location.href = '/';
-    }
+    testTab = this._root.query("#test-tab");
+    practiceTab = this._root.query("#practice-tab");
+    window.onHashChange.listen(handleHashChange);
   }
   
   void onLogoutClick(MouseEvent event) {
     UserManager.logout();
+  }
+  
+  handleHashChange(Event e) {
+    if (window.location.hash == '#test') {
+      testTab.classes.add("active");
+      practiceTab.classes.remove("active");
+    } else if (window.location.hash == '#practice') {
+      testTab.classes.remove("active");
+      practiceTab.classes.add("active");
+    } else if (window.location.hash == '#select-cn') {
+      Messages.setLanguage('ch');
+    } else if (window.location.hash == '#select-en') {
+      Messages.setLanguage('en');
+    } else if (window.location.hash == '#') {
+      // ignore.
+    }
   }
 }
