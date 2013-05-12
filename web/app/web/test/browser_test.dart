5b6bad93d577909736046eb86c2713e955525a3b
Nicholas <nicholas.zhaoyu@gmail.com>
2013-05-12 11:51:42 -0700

    Test failue.

--- web/app/web/test/browser_test.dart
+++ web/app/web/test/browser_test.dart
@@ -6,4 +6,5 @@ import 'lib/models/readingsection_test.dart';
 void main() {
   test("Construction", () => expect(create(), 1));
   test("Construction", () => expect(create(), 2));
+  print("unittest-suite-done");
 }
