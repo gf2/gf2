29d415e56a19b2e7f5e9b78f106ac9169f87342c
Nicholas <nicholas.zhaoyu@gmail.com>
2013-05-12 21:24:14 -0700

    Use html config.

--- web/app/web/test/browser_test.dart
+++ web/app/web/test/browser_test.dart
@@ -1,10 +1,12 @@
 library browser_test;
+import 'package:unittest/html_enhanced_config.dart';
 import 'dart:async';
 import 'dart:html';
 import 'package:unittest/unittest.dart';
 import 'lib/models/readingsection_test.dart';
 
 void main() {
+  useHtmlEnhancedConfiguration();
   test("Construction", () => expect(create(), 1));
   test("Construction", () => expect(create(), 2));
   print("unittest-suite-done");
