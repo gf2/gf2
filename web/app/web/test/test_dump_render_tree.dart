f450812f1bc9cc3d016dfb432e164d8eb3881e84
Nicholas <nicholas.zhaoyu@gmail.com>
2013-05-12 21:59:46 -0700

    Add test dump render tree.

---
+++ web/app/web/test/test_dump_render_tree.dart
@@ -0,0 +1,36 @@
+library test_dump_render_tree;
+
+import 'dart:async';
+import 'dart:io';
+import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
+
+main() {
+  testCore(new VMConfiguration());
+}
+
+void testCore(Configuration config) {
+  unittestConfiguration = config;
+  groupSep = ' - ';
+  final browserTests = ['browser_test.html'];
+
+  group('DumpRenderTree', () {
+    browserTests.forEach((file) {
+      test(file, () => _runDrt(file));
+    });
+  });
+}
+
+Future _runDrt(String htmlFile) {
+  final allPassedRegExp = new RegExp('All \\d+ tests passed');
+
+  return Process.run('DumpRenderTree', [htmlFile])
+    .then((ProcessResult pr) {
+      expect(pr.exitCode, 0, reason: 'DumpRenderTree should return exit code 0 - success');
+
+      if(!allPassedRegExp.hasMatch(pr.stdout)) {
+        print(pr.stdout);
+        fail('Could not find success value in stdout: ${allPassedRegExp.pattern}');
+      }
+    });
+}
